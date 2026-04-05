c6e9736aee5c541c2fcb37c0558417819d0e27fc
Shashank Mishra <shashwatmisra786@gmail.com>
2026-04-05 21:19:31 +0530

    Food List Completed

--- frontend/src/pages/FoodList.js
+++ frontend/src/pages/FoodList.js
@@ -1,45 +1,149 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import PublicLayeout from '../components/PublicLayout'
+import '../styles/Home.css'
 import { Link } from 'react-router-dom';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 const FoodList = () => {
     const [foods, setFoods] = useState([]);
-    useEffect(()=>{
+    const [filteredFoods, setFilteredFoods] = useState([]);
+    const [search, setSearch] = useState('');
+    const [categories, setCategories] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState('All');
+    const [minPrice, setMinPrice] = useState(0);
+    const [maxPrice, setMaxPrice] = useState(500);
+    const [currentPage, setCurrentPage] = useState(1);
+    const foodPerPage = 9;
+    useEffect(() => {
         fetch(`http://127.0.0.1:8000/api/foods/`)
             .then(res => res.json())
             .then(data => {
-                setFoods(data)
-            })
-    },[]);
-  return (
-    <PublicLayeout>
-        <div className='row mt-4'>
-            {foods.length===0 ? (<p className='text-center'>No Food Found.</p>) : (
-                foods.map((food)=>(
-                    <div className='col-md-4 mb-4'>
-                    <div className="card hovereffect">
-                        <img src={`http://127.0.0.1:8000${food.image}`} className='card-img-top' style={{ height: '200px', objectFit: 'cover' }} alt="food_img" />
-                        <div className='card-body'>
-                            <h5 className='card-title'>
-                                <Link to={`/food/${food.id}`}>{food.item_name}</Link>
-                            </h5>
-                            <p className='card-text text-muted'>{food.item_description?.slice(0,40)} {food.item_description?.length>40 && '...'}</p>
-                            <div className='d-flex justify-content-between align-items-center'>
-                                <span className='fw-bold'>₹ {food.item_price}</span>
-                                {food.is_available ? (
-                                    <Link to={`/food/${food.id}`} className='btn btn-outline-primary btn-sm'><i className='fas fa-shopping-basket me-1'></i> Order Now</Link>
-                                ) : (
-                                    <div title='This food item is not availabl right now. Please try again later.'><button className='btn btn-outline-secondary btn-sm'><i className='fas fa-times-circle me-1'></i>Currently Unavailable</button></div>
-                                ) }
+                setFoods(data);
+                setFilteredFoods(data);
+            });
+        fetch(`http://127.0.0.1:8000/api/categories/`)
+            .then(res => res.json())
+            .then(data => {
+                setCategories(data);
+            });
+    }, []);
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        applyFilters(search,selectedCategory);
+    }
+    
+    const handleCategoryChange = (e) => {
+        const category = e.target.value;
+        setSelectedCategory(category);
+        applyFilters(search,category);
+    }
+
+    const applyFilters = (searchTerm,selectedCategory) => {
+        let result = foods;
+        if (searchTerm) {
+            result = result.filter(food=>food.item_name.toLowerCase().includes(searchTerm.toLowerCase()));
+        };
+        if (selectedCategory != 'All') {
+            result = result.filter(food=>food.category_name==selectedCategory);
+        };
+        result = result.filter(food=>food.item_price>=minPrice && food.item_price<=maxPrice);
+        setFilteredFoods(result);
+        setCurrentPage(1);
+    };
+
+    //Pagination Logic
+    const indexOfLastFood = currentPage * foodPerPage;
+    const indexOfFirstFood = (currentPage - 1) * foodPerPage;
+    const currentFoods = filteredFoods.slice(indexOfFirstFood,indexOfLastFood);
+    const totalPages = Math.ceil(filteredFoods.length / foodPerPage);
+
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+    return (
+        <PublicLayeout>
+            <div class="container py-5">
+                <h2 class="text-center mb-4">Find Your Delicious Food Here...</h2>
+                <div class="row mb-4">
+                    <div class="col-md-8">
+                        <form onSubmit={handleSearch}>
+                            <div class="input-group">
+                                <input type="text" className='form-control' placeholder='Search your favourite food' value={search} onChange={(e) => setSearch(e.target.value)}/>
+                                <button class="btn btn-primary" type='submit'><i className='fa fa-search'></i></button>
                             </div>
-                        </div>
+                        </form>
                     </div>
+                    <div class="col-md-4">
+                        <select class="form-select" value={selectedCategory} onChange={handleCategoryChange}>
+                            <option value="All">All Categories</option>
+                            {categories.map((cat)=>(
+                                <option key={cat.id} value={cat.category_name}>{cat.category_name}</option>
+                            ))}
+                        </select>
+                    </div>
+                </div>
+                <div class="row mb-4">
+                    <div class="col-md-12">
+                        <label for="" class="form-label fw-bold my-2">Filter by Price: ₹{minPrice} - ₹{maxPrice}</label>
+                        <Slider range min={0} max={500} defaultValue={[minPrice,maxPrice]} onChange={(value)=>{
+                            setMinPrice(value[0]);
+                            setMaxPrice(value[1]);
+                            applyFilters(search,selectedCategory);
+                        }}>
+                            
+                        </Slider>
+                    </div>
+                </div>
+                <div className='row mt-4'>
+                    {currentFoods.length === 0 ? (<p className='text-center'>No Food Found.</p>) : (
+                        currentFoods.map((food) => (
+                            <div className='col-md-4 mb-4'>
+                                <div className="card hovereffect">
+                                    <img src={`http://127.0.0.1:8000${food.image}`} className='card-img-top' style={{ height: '200px', objectFit: 'cover' }} alt="food_img" />
+                                    <div className='card-body'>
+                                        <h5 className='card-title'>
+                                            <Link to={`/food/${food.id}`}>{food.item_name}</Link>
+                                        </h5>
+                                        <p className='card-text text-muted'>{food.item_description?.slice(0, 40)} {food.item_description?.length > 40 && '...'}</p>
+                                        <div className='d-flex justify-content-between align-items-center'>
+                                            <span className='fw-bold'>₹ {food.item_price}</span>
+                                            {food.is_available ? (
+                                                <Link to={`/food/${food.id}`} className='btn btn-outline-primary btn-sm'><i className='fas fa-shopping-basket me-1'></i> Order Now</Link>
+                                            ) : (
+                                                <div title='This food item is not availabl right now. Please try again later.'><button className='btn btn-outline-secondary btn-sm'><i className='fas fa-times-circle me-1'></i>Currently Unavailable</button></div>
+                                            )}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    )}
+                </div>
+                {totalPages > 1 && (
+                    <nav class="mt-4 d-flex justify-content-center">
+                        <ul className='pagination'>
+                            <li className={`page-item ${currentPage === 1 && 'disabled'}`}>
+                                <button className='page-link' onClick={()=>paginate(1)}>First</button>
+                            </li>
+                            <li className={`page-item ${currentPage === 1 && 'disabled'}`}>
+                                <button className='page-link' onClick={()=>paginate(currentPage-1)}>Prev</button>
+                            </li>
+                            <li className='page-item disabled'>
+                                <button className='page-link'>Page {currentPage} of {totalPages}</button>
+                            </li>
+                            <li className={`page-item ${currentPage === totalPages && 'disabled'}`}>
+                                <button className='page-link' onClick={()=>paginate(currentPage+1)}>Next</button>
+                            </li>
+                            <li className={`page-item ${currentPage === totalPages && 'disabled'}`}>
+                                <button className='page-link' onClick={()=>paginate(totalPages)}>Last</button>
+                            </li>
+                        </ul>
+                    </nav>
+                )}
             </div>
-                ))
-            ) }
-        </div>
-    </PublicLayeout>
-  );
+
+        </PublicLayeout>
+    );
 }
 
 export default FoodList;
